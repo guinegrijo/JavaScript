5443541c3f392b3181c76efeae032abfb1a3a739
guinegrijo <guilhermenegrijo@gmail.com>
2024-07-31 11:12:56 -0300

    Exercicios_JS

---
+++ exercicio01.js
@@ -0,0 +1,8 @@
+var idade = 18
+if(idade < 18 && idade >=0){
+    console.log("Menor de idade")
+} else if (idade >= 18){
+    console.log('Maior de idade')
+} else{
+    console.log('Digite uma idade natural')
+}
---
+++ exercicio02.js
@@ -0,0 +1,10 @@
+var num1 = 10
+var num2 = 1
+
+if (num1 > num2){
+    console.log(`O ${num1} é o maior número`)
+} else if (num1 < num2){
+    console.log(`O ${num2} é o maior número`)
+} else {
+    console.log('Os números são iguais')
+}
