4152b7f245e7b486a9df2bc99eddf90e55e20459
guinegrijo <guilhermenegrijo@gmail.com>
2024-08-21 10:58:03 -0300

    Exercicio API

---
+++ API-Exercicio/src/server.js
@@ -0,0 +1,4 @@
+//importar a instancia do express congiurada em index
+const app = require("./index")
+//inicia o servidor na porta 3000, nesse caso a api sera acessivel em http://localhost:3000/
+app.listen(3000)
